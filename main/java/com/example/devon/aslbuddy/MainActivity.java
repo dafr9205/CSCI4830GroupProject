099a0551901b5501aea6bf03ee13b0e934c57448
BryanOhagan <broh8605@colorado.edu>
2017-03-19 16:12:23 -0600

    Add files via upload

---
+++ main/java/com/example/devon/aslbuddy/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.devon.aslbuddy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    public void OnClickButtonListener() {
+        Button button = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, alphabetActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+        button2.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, numActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+        button3.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, basicActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OnClickButtonListener();
+
+    }
+
+
+
+}
