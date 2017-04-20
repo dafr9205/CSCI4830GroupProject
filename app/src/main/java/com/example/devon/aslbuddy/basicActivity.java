77b40094ae9480e9ab0ab9ad36c0eeab54de5589
dafr9205 <dafr9205@colorado.edu>
2017-04-19 18:59:23 -0600

    Added working link to startASL basic words in activity_basic

--- app/src/main/java/com/example/devon/aslbuddy/basicActivity.java
+++ app/src/main/java/com/example/devon/aslbuddy/basicActivity.java
@@ -1,10 +1,11 @@
 package com.example.devon.aslbuddy;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
-
-
 
 
 public class basicActivity extends AppCompatActivity {
@@ -15,7 +16,10 @@ public class basicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_basic);
     }
 
-
+    public void browser1(View view ){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.startasl.com/basic-words-in-sign-language.html"));
+        startActivity(browserIntent);
+    }
 }
 
 
